72ee05af7cde39cf89d654c01debe59ef0e874dc
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2025-02-23 20:14:20 -0500

    refactor ServantSelection component to set fixed dimensions for grid items and improve layout

--- src/components/ServantSelection.js
+++ src/components/ServantSelection.js
@@ -5,10 +5,22 @@ import ServantAvatar from './ServantAvatar';
 const ServantSelection = ({ servants, handleServantClick }) => {
   return (
     <div>
-      <Typography variant="h6" style={{marginBottom:"1rem"}}>Servant Selection</Typography>
+      <Typography variant="h6" style={{ marginBottom: "1rem" }}>Servant Selection</Typography>
       <Grid container spacing={2} style={{ width: '40rem', height: '70vh', overflowY: 'auto' }}>
         {servants.map((servant, index) => (
-          <Grid item xs={4} key={index} onClick={() => handleServantClick(servant)}>
+          <Grid
+            item
+            xs={4}
+            key={index}
+            onClick={() => handleServantClick(servant)}
+            sx={{
+              width: '128px', // Set the width to 128px or 144px as needed
+              height: '128px', // Set the height to 128px or 144px as needed
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
             <ServantAvatar
               servantFace={servant.extraAssets?.faces?.ascension?.['4']}
               bgType={servant.noblePhantasms?.['0']?.card}
